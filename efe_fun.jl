871a26bf732f140780124f7acf8ddfa08a0d0736
MagnusKoudahl <magnuskoudahl@gmail.com>
2020-08-25 13:51:50 +0200

    Update EFE function. Compute for simple prediction with linear filter

--- efe_fun.jl
+++ efe_fun.jl
@@ -7,21 +7,24 @@ using LinearAlgebra,Distributions,PDMats
 # Where the first term is a crossentropy. All terms can be solved analytically for linear gaussian systems, giving an analytical solution to EFE in continuous time
 
 
-# Calculate EFE. Takes p(o), q(s) and likelihood variance of p(o|s) as input
-function efe(p_o,q_s,Σ_p_o_given_s)
+# Calculate EFE.
+function efe(q_s_given_o,q_s,p_o,Σ_p_o_given_s)
+    # q_s_given_o = q(s|o), can be obtained from Kalman update step
+    # q_s = q(s), can be obtained from Kalman prediction step
+    # p_o = p(o|m), goal prior mean. We only supply the mean, precision is calculated from generative model
+    # Σ_p_o_given_s = R, likelihood variance. Known from model specification
+# This implementation assumes H = A = I, ie all transition models are just the identity matrix
 
-    # Compute the posterior q(s | o) after observing mean of p_o
-    q_s_given_o = MvNormal(p_o.Σ.mat * inv(p_o.Σ.mat + q_s.Σ.mat) * q_s.μ + q_s.Σ.mat * inv(p_o.Σ.mat + q_s.Σ.mat) * p_o.μ, p_o.Σ.mat * inv(p_o.Σ.mat + q_s.Σ.mat) * q_s.Σ.mat)
-
-    # Compute q_tilde as p(o|s)q(s)
+    # Compute q_tilde as p(o|s)q(s). This does not use the goal prior
     q_tilde = MvNormal(cat(q_s.μ,q_s.μ,dims=1),
 		       [q_s.Σ.mat q_s.Σ.mat ;
 			q_s.Σ.mat Σ_p_o_given_s + q_s.Σ.mat])
 
-    # Compute p(o,s) as q(s|o)p(o)
-    p_os = MvNormal(cat(p_o.μ,p_o.μ,dims=1),
-		    [p_o.Σ.mat p_o.Σ.mat ;
-		     p_o.Σ.mat q_s_given_o.Σ + p_o.Σ.mat])
+
+    # Compute p(o,s) as q(s|o)p(o). This uses the goal prior. Currently relies heavily on H = inv(H) = I
+    p_os = MvNormal([p_o;p_o],
+		    [q_s_given_o.Σ.mat + Σ_p_o_given_s q_s_given_o.Σ.mat + Σ_p_o_given_s ;
+		     q_s_given_o.Σ.mat + Σ_p_o_given_s q_s_given_o.Σ + q_s_given_o.Σ.mat + Σ_p_o_given_s])
 
     # Energy as crossentropy of q_tilde and p_os
     energy = 0.5 * log(2*π) + logdet(q_tilde.Σ) + invquad(q_tilde.Σ,q_tilde.μ) - transpose(p_os.μ)*(q_tilde.Σ \ q_tilde.μ) - transpose(q_tilde.μ)* (q_tilde.Σ \ p_os.μ) + tr(q_tilde.Σ \ (p_os.μ * transpose(p_os.μ) + p_os.Σ))
--- efe_kalman_filter.jl
+++ efe_kalman_filter.jl
@@ -1,13 +1,7 @@
 using Pkg;Pkg.activate(".");Pkg.instantiate()
 using OhMyREPL, Plots, Distributions
 enable_autocomplete_brackets(false)
-include("./efe_fun_2.jl")
-
-# Generate data from a noisy sin function
-function generate_sin_data(minimum,maximum,stepsize,σ)
-    [sin.([x;x]) + rand(MvNormal([0.;0.],σ)) for x in minimum:stepsize:maximum]
-end
-data = generate_sin_data(0,10,0.1,0.1)
+include("./efe_fun.jl")
 
 # Prediction step
 function kalman_predict(prior, A,Q)
@@ -22,37 +16,46 @@ function kalman_update(prior, y, H, R)
     MvNormal(prior.μ + K * v, prior.Σ - K * S * transpose(K))
 end
 
+# A and H have to be I right now
 # Transition matrix and process noise
 A = [1. 0. ; 0. 1.]
 Q = [0.1 0. ; 0. 0.1]
 
 # Emission matrix and process noise
 H = [1. 0. ; 0. 1.]
-R = [1.1 0. ; 0. 1.1]
+R = [1. 0. ; 0. 1.]
 
-# Goal prior
-p_o= MvNormal([10.,10.],[0.1 0. ; 0. 0.1])
+# Goal prior. μ only since precision is given by generative model
+p_o = [10.,10.]
 
 # Initial state
-#global prior_q = MvNormal([0.,0.], [0.1 0. ; 0. 0.1])
 global prior = MvNormal([0.,0.], [0.1 0. ; 0. 0.1])
 
 # Horizon
 T=10
 
 # Storage and simulation loop
-x = Vector(undef,length(data))
-q_s = Vector(undef,T)
-q_s_given_o = Vector(undef,T)
+q_s_vec = Vector(undef,T)
+q_s_given_o_vec = Vector(undef,T)
 
 efe_vec = Vector(undef,T)
 
 for t ∈ 1:T
-    prediction = kalman_predict(prior, A,Q)
-    q_s[t] = prediction
-    global prior = prediction
-    efe_vec[t] = efe(p_o,prediction,R)
-    #global prior = kalman_update(prediction, data[t], H, R)
-    #x[t] = prior
+    # Calculate q(s)
+    q_s = kalman_predict(prior, A,Q)
+    # Calculate q(s|o)
+    q_s_given_o = kalman_update(q_s, p_o, H, R)
+
+    # Calculate EFE and store
+    efe_vec[t] = efe(q_s_given_o, q_s, p_o, R)
+    q_s_given_o_vec[t] = q_s_given_o
+    q_s_vec[t] = q_s
+
+    global prior = q_s
 end
-efe_vec
+
+plot(1:T,efe_vec)
+plot(1:T, [x.μ[1] for x in q_s_vec],ribbon=[x.Σ[1] for x in q_s_vec])
+plot!(1:T,[x.μ[2] for x in q_s_vec],ribbon=[x.Σ[4] for x in q_s_vec])
+plot!(1:T,[x.μ[1] for x in q_s_given_o_vec],ribbon=[x.Σ[1] for x in q_s_given_o_vec])
+plot!(1:T,[x.μ[2] for x in q_s_given_o_vec],ribbon=[x.Σ[4] for x in q_s_given_o_vec])
